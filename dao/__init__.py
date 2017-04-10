a384af5065fe6369c9836e7c398c9e290d294a1e
Fredy Ramos <fredy.rx@gmail.com>
2017-04-09 23:57:00 -0500

    add db connection methods

---
+++ dao/__init__.py
@@ -0,0 +1,64 @@
+import psycopg2
+from settings import config_db
+
+class QueryResponse(object):
+	def __init__(self,data=[],error=False,message=""):
+		self.data = data
+		self.error = error
+		self.message = message
+
+	def set_data(self,data):
+		self.data = data
+
+	def set_error(self):
+		self.error = True
+
+	def set_message(self, message):
+		self.message = message
+
+	def get_dict(self):
+		return self.__dict__
+
+def get_connection():
+	params = config_db()
+	return psycopg2.connect(**params)
+
+def basic_callproc(sp_name,params=list()):
+	conn = None
+	result = QueryResponse()
+	try:
+		conn = get_connection()
+		cur = conn.cursor()
+		cur.callproc(sp_name,params)
+		# Procesamos el resultado
+		rows = cur.fetchall()
+		result.set_data(rows)
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)
+		result.set_error()
+		result.set_message(error)
+	finally:
+		if conn is not None:
+			conn.close()
+
+	return result.get_dict()
+
+def exec_query(query,params=list()):
+	conn = None
+	result = QueryResponse()
+	try:
+		conn = get_connection()
+		cur = conn.cursor()
+		cur.execute(query,params)
+		# Procesamos el resultado
+		rows = cur.fetchall()
+		result.set_data(rows)
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)
+		result.set_error()
+		result.set_message(error)
+	finally:
+		if conn is not None:
+			conn.close()
+
+	return result.get_dict()
--- settings.py
+++ settings.py
@@ -3,3 +3,15 @@
 SECRET_KEY = "!&nq(vhsj^mskm)ja3qow9_r_=sm)!1aka9^_$r#z!ua+@3vel"
 EXPIRATION_TOKEN = 60*60*24 #  (1 min) * 60*24 = 1 dia
 JSON_AS_ASCII = False
+
+# DataBase settings
+CONFIG_DB = {
+	'host': '127.0.0.1',
+	'port': 5432,
+	'database': "movilidadDB", 
+	'user': "postgres", 
+	'password': "postgres"
+}
+
+def config_db():
+	return CONFIG_DB
